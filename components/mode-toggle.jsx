f363f7ea66034c4aa3a8f1bb32b6090173b1b6ef
Malik <mmalith520@gmail.com>
2025-04-05 17:14:58 +0530

    button mode change

--- components/mode-toggle.jsx
+++ components/mode-toggle.jsx
@@ -4,7 +4,7 @@ import { Sun } from "lucide-react";
 
 
 export  function ModeToggle() {
-  return  <Button variant="ghost" size="icon" className="w-9 h- p9-2 rounded-full hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"> 
+  return  <Button variant="ghost" size="icon" className="w-9 h-9"> 
     <Sun className="h-[1.2rem] w-[1.2rem]"/>
   </Button>;
 }
