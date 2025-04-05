544a879a7ba1f08d228a5447627fc67045f1c4cc
Malik <mmalith520@gmail.com>
2025-04-05 17:00:20 +0530

    install shadcns

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 // SSR - Server Side Rendered - Server Component
-import HeaderNav from '@/components/header-nav';
+import HeaderNav from '@/components/header-nav'; //alias import
+
+
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeaderNav />
-      <main className=" bg-primary h-screen">Main Section</main>
+      <main className="bg-purple-200 h-screen">Main Section</main>
       <footer className=" bg-amber-400 h-72"> Footer Section</footer>
     </div>
   );
--- components/header-nav.jsx
+++ components/header-nav.jsx
@@ -1,10 +1,12 @@
 
 import Link from "next/link"
+import { Logo } from "./logo"
 export default function HeaderNav() {
   return (
     <header className="border-primary/20 bg-background sticky top-0 z-50 w-full border-b">
         <div className="container flex h-16 items-center">
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-3">
+        <Logo className="h-8 w-8" />
           <span className="text-primary text-xl font-bold">CineScope</span>
         </Link>
 
