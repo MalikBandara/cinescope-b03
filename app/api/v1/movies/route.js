cb606d539fd376e9e4f45fb2e0fabe7de29ecb03
Malik <mmalith520@gmail.com>
2025-04-27 13:06:49 +0530

    done hero banner and movies list loaded day 8

---
+++ app/api/v1/movies/route.js
@@ -0,0 +1,8 @@
+export const GET = async () =>{
+
+    try{
+        // const res = await fetch ('https://api.themoviedb.org/3/movie/popular?api_key=YOUR_API_KEY&language=en-US&page=1', )
+    }catch( error ){
+        
+    }
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,16 @@
 // SSR - Server Side Rendered - Server Component
-import HeaderNav from '@/components/header-nav'; //alias import
-
-
+import HeaderNav from "@/components/header-nav"; //alias import
+import FeaturedMovies from "@/components/home/featured-moives";
+import HeroBanner from "@/components/home/hero-banner";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeaderNav />
-      <main className="bg-purple-200 h-screen">Main Section</main>
+      <main className="flex-1">
+        <HeroBanner />
+        <FeaturedMovies />
+      </main>
       <footer className=" bg-amber-400 h-72"> Footer Section</footer>
     </div>
   );
