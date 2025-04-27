28da7081f29b77f298141a99bee7cf46b3487825
Malik <mmalith520@gmail.com>
2025-04-27 14:42:37 +0530

    error handling  and back end front end done

--- app/api/v1/movies/route.js
+++ app/api/v1/movies/route.js
@@ -1,8 +1,15 @@
+import { Movies } from "@/lib/data";
+import { NextResponse } from "next/server";
+
+
+//our first api ro
 export const GET = async () =>{
 
     try{
         // const res = await fetch ('https://api.themoviedb.org/3/movie/popular?api_key=YOUR_API_KEY&language=en-US&page=1', )
+        return NextResponse.json(Movies, {status : 200 });
     }catch( error ){
-        
+        console.log("Error fetching movies", error);
+        return NextResponse.json({error: "Internal server error"}, {status : 500 });
     }
-}
+};
--- components/home/featured-moives.jsx
+++ components/home/featured-moives.jsx
@@ -1,5 +1,8 @@
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import MoviesList from "./movies-list";
+import { Suspense } from "react";
 
 export default function FeaturedMovies() {
   return (
@@ -17,8 +20,26 @@ export default function FeaturedMovies() {
         <Button variant="outline">View All </Button>
       </div>
 
-      <MoviesList/>
-
+      <div className="space-y-6">
+        <div className="border-primary/20 bg-card shadow-xs rounded-lg border p-4 ">
+          <div className="flex flex-col gap-4 ">
+            <div className="relative">
+              <Search className="text-primary/70 absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
+              <Input
+                placeholder="Search movies by title or director"
+                className="border-primary/20 pl-9 "
+              />
+            </div>
+          </div>
+        </div>
+        <Suspense
+          fallback={
+            <div className="h-96 animate-pulse rounded-lg bg-muted"></div>
+          }
+        >
+          <MoviesList />
+        </Suspense>
+      </div>
     </section>
   );
 }
