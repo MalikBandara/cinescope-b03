2918adf4203296dbc807a87b95a74f11409fb0bc
MalikBandara <mmalith520@gmail.com>
2025-06-26 23:21:20 +0530

    last change

--- actions/movies.js
+++ actions/movies.js
@@ -105,7 +105,7 @@ export const searchMovies = async query => {
   try {
     //
     const movies = await db
-      .collection('movies')
+      .collection('movies_n')
       .find({ title: { $regex: query, $options: 'i' } })
       .limit(50)
       .toArray()
